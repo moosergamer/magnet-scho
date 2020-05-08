7fe455c5f639612ad3e0f160002f6d2e734a14d5
moosergamer <63883210+moosergamer@users.noreply.github.com>
2020-05-08 07:54:21 +0400

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "1008363308:AAGocFkVHrQdzfwz1Z_s7_a-Fdo7BXjY4sw"
-bot_username = "magnet_bot"
-URL = "https://telegram-magnet-bot.herokuapp.com/"
+bot_token = "1237970786:AAHeY9AS18yTJuS3SkwjiD5EvoK9cOtoMKo"
+bot_username = "Mooserproxy_bot"
+URL = "https://moosersearch.herokuapp.com/"
